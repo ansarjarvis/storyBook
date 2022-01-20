eea362756d05b516cfa7c9e7d0b4b9424854bef3
ansarjarvis <ansar.ofcl00@gmail.com>
2022-01-20 13:21:24 +0530

    fixing error

--- app.js
+++ app.js
@@ -100,5 +100,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("server live at port 3000")
+    console.log(`server live at  port ${port}`)
 })
